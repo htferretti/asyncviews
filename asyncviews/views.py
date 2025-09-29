9927443039e614c69d8db0411f02f13663637902
htferretti <htferretti@gmail.com>
2025-09-29 19:03:43 -0300

    first commit

---
+++ asyncviews/views.py
@@ -0,0 +1,24 @@
+import time, asyncio, httpx
+from django.http import JsonResponse, HttpResponse
+
+def api(request):
+    time.sleep(1)
+    payload = {"message": "Hello motherfucker!"}
+
+    if "task_id" in request.GET:
+        payload["task_id"] = request.GET["task_id"]
+    return JsonResponse(payload)
+
+
+async def http_call_async():
+    for num in range(1, 6):
+        await asyncio.sleep(1)
+        print(num)
+    async with httpx.AsyncClient() as client:
+        r = await client.get("https://httpbin.org/")
+        print(r)
+
+async def async_view(request):
+    loop = asyncio.get_event_loop()
+    loop.create_task(http_call_async())
+    return HttpResponse("Non-blocking HTTP request")
